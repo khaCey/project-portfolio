3f8ccb8bd01239dae7d893f25e63683d6e4c2c11
Khacey Salvador <khacey.salvador@gmail.com>
2022-12-10 00:09:54 +0900

    none

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
-import { useContext } from "react";
 import Home from './Pages/HomePage/Home.js';
-import Search from './Pages/SearchBar/SearchPage';
-import { PageContext } from "./Pages/Variables.js";
 function App() {
-  const { currentPage, setCurrentPage } = useContext(PageContext);
   return (
     <div>
         <Home />
